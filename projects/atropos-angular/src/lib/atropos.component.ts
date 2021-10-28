22547fe47a8b68aa33265590c9747909ba5e2763
sytzez <sytzez@gmail.com>
2021-10-28 22:05:35 +0200

    Initialize Atropos after view init

--- projects/atropos-angular/src/lib/atropos.component.ts
+++ projects/atropos-angular/src/lib/atropos.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import Atropos, { AtroposInstance } from 'atropos';
 
 @Component({
     selector: 'atropos',
     templateUrl: 'atropos.component.html',
     styleUrls: ['atropos.component.scss'],
 })
-export class AtroposComponent implements OnInit {
+export class AtroposComponent implements AfterViewInit {
 
-    constructor() {
-    }
+    @ViewChild('element', { static: true })
+    protected element!: HTMLDivElement;
 
-    ngOnInit(): void {
+    protected atropos?: AtroposInstance;
+
+    public ngAfterViewInit(): void {
+        this.atropos = Atropos({
+            el: this.element,
+        });
     }
 
 }
