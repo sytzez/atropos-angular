1b7958a05db2e4a4a21606b0a5ef0c9e09c792b0
sytzez <sytzez@gmail.com>
2021-10-29 21:32:51 +0200

    Test events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'atropos-angular-workspace';
+
+    public log(event: any): void {
+        console.log(event);
+    }
 }
