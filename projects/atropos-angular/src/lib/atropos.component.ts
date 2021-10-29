2cde98b4fa6ba20633064d0e3f57f4f42953ba7f
sytzez <sytzez@gmail.com>
2021-10-29 21:32:08 +0200

    Implement all params and events

--- projects/atropos-angular/src/lib/atropos.component.ts
+++ projects/atropos-angular/src/lib/atropos.component.ts
@@ -1,5 +1,12 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, Output, ViewChild } from '@angular/core';
 import Atropos, { AtroposInstance } from 'atropos';
+
+export interface RotateEvent {
+    x: number;
+    y: number;
+}
+
+export type RotateTouch = boolean | 'scroll-x' | 'scroll-y';
 
 @Component({
     selector: 'atropos',
@@ -7,6 +14,66 @@ import Atropos, { AtroposInstance } from 'atropos';
     styleUrls: ['atropos.component.scss'],
 })
 export class AtroposComponent implements AfterViewInit, OnDestroy {
+
+    @Input()
+    protected activeOffset: number = 50;
+
+    @Input()
+    protected alwaysActive: boolean = false;
+
+    @Input()
+    protected duration: number = 300;
+
+    @Input()
+    protected rotate: boolean = true;
+
+    @Input()
+    protected rotateTouch: RotateTouch = true;
+
+    @Input()
+    protected rotateXMax: number = 15;
+
+    @Input()
+    protected rotateYMax: number = 15;
+
+    @Input()
+    protected rotateXInvert: boolean = false;
+
+    @Input()
+    protected rotateYInvert: boolean = false;
+
+    @Input()
+    protected stretchX: number = 0;
+
+    @Input()
+    protected stretchY: number = 0;
+
+    @Input()
+    protected stretchZ: number = 0;
+
+    @Input()
+    protected commonOrigin: boolean = true;
+
+    @Input()
+    protected shadow: boolean = true;
+
+    @Input()
+    protected shadowOffset: number = 50;
+
+    @Input()
+    protected shadowScale: number = 1;
+
+    @Input()
+    protected highlight: boolean = true;
+
+    @Output()
+    protected onEnter = new EventEmitter<void>();
+
+    @Output()
+    protected onLeave = new EventEmitter<void>();
+
+    @Output()
+    protected onRotate = new EventEmitter<RotateEvent>();
 
     @ViewChild('element', { static: true })
     protected element!: ElementRef<HTMLDivElement>;
@@ -16,6 +83,26 @@ export class AtroposComponent implements AfterViewInit, OnDestroy {
     public ngAfterViewInit(): void {
         this.atropos = Atropos({
             el: this.element.nativeElement,
+            activeOffset: this.activeOffset,
+            alwaysActive: this.alwaysActive,
+            duration: this.duration,
+            rotate: this.rotate,
+            rotateTouch: this.rotateTouch,
+            rotateXMax: this.rotateXMax,
+            rotateYMax: this.rotateYMax,
+            rotateXInvert: this.rotateXInvert,
+            rotateYInvert: this.rotateYInvert,
+            stretchX: this.stretchX,
+            stretchY: this.stretchY,
+            stretchZ: this.stretchZ,
+            commonOrigin: this.commonOrigin,
+            shadow: this.shadow,
+            shadowOffset: this.shadowOffset,
+            shadowScale: this.shadowScale,
+            highlight: this.highlight,
+            onEnter: () => this.onEnter.emit(),
+            onLeave: () => this.onLeave.emit(),
+            onRotate: (x, y) => this.onRotate.emit({ x, y }),
         });
     }
 
